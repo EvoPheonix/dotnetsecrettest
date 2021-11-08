ed11b1a835b433caa01756adfc6a36d9a8e1daf9
Sheldon McGrath <Sheldon.McGrath@priv.gc.ca>
2021-11-08 15:27:59 -0500

    test

--- dotnetsecrettest/Program.cs
+++ dotnetsecrettest/Program.cs
@@ -13,6 +13,8 @@ namespace dotnetsecrettest
     {
         public static void Main(string[] args)
         {
+            var APIKey = "ENbgIgbWFXyoV0bwxsDCNmL9+rUxNY5U9dTRUaXChQSOpTuKTXa12dNhwP8gH/sQZ9GgUT2qV71H3CwqsWPMGw==";
+
             CreateHostBuilder(args).Build().Run();
         }
 
